bb2342d57a7fc21d232c4efde1b13d7769fcff61
Noahhai22 <93018649+Noahhai22@users.noreply.github.com>
2024-11-24 01:20:56 -0800

    Delete .pg-migrate-config.js
    didnt need this

--- .pg-migrate-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    databaseUrl: process.env.DATABASE_URL || 'postgresql://noahhai:Acmc2201291!@localhost:5432/mydatabase',
-    migrationsTable: 'pgmigrations',
-    schema: 'public',
-    dir: 'migrations',
-  };
-  
